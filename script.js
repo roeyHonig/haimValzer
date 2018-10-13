97a116b3fea766c00acad804c93c7c10756e0ecc
Roey Honig <roeyhonig94@gmail.com>
2018-10-13 14:27:19 +0300

    need to figureout how to load content using the Ajax

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
 $(document).ready(function(){
    console.log("hello world")
 
-   
 
-   $("#flipbook").turn({
-    /*width: $("#bookContainer").width(),*/
-    /*height: $("#bookContainer").height(),*/
+   $(".flipbook").turn({
+    /*width: 1200,*/
+    /*height: 600,*/
     elevation: 50,
+    pages:8 ,
     autoCenter: true,
-    duration: 1000
-    
+    duration: 3000,
+    when: {
+        missing: function (e, pages) {
+            console.log("missing")
+            for (var i = 0; i < pages.length; i++) {
+                addPage(pages[i], $(this));
+            }
+
+        }
+    }
     
 });
+
+
+function addPage(page, book) {
+
+	var id, pages = book.turn('pages');
+
+	if (!book.turn('hasPage', page)) {
+
+		var element = $('<div />',
+			{'class': 'own-size',
+				css: {width: 400, height: 582}
+			}).
+			html('<div class="loader">Hello Missing Page</div>');
+
+		if (book.turn('addPage', element, page)) {
+			loadPage(page);
+		}
+
+	}
+}
+
+function loadPage(page) {
+
+	$.ajax({url: 'pages/page' + page + '.html'});
+
+}
+
+
+
 
 
 
