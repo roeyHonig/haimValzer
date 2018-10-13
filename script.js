905b36ad37d87dbfca69ee7c5aa6fa0b73edbcf0
Roey Honig <roeyhonig94@gmail.com>
2018-10-13 10:23:20 +0300

    turn JS

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+   console.log("hello world")
+
+   
+
+   $("#flipbook").turn({
+    /*width: $("#bookContainer").width(),*/
+    /*height: $("#bookContainer").height(),*/
+    elevation: 50,
+    autoCenter: true,
+    duration: 1000
+    
+    
+});
+
+
+
+
+
+});
